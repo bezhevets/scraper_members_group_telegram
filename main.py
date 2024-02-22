9bb81d07159443cc26475c1b8ede1ddec29ea7b3
Maksym Bezhevets <begik4@gmail.com>
2024-02-22 12:35:25 +0200

    Added functionality to get all participants from the selected group.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from dataclasses import dataclass, fields
+from pprint import pprint
 
 from telethon import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
@@ -15,6 +17,19 @@ API_HASH = os.environ["API_HASH"]
 client = TelegramClient("anon", API_ID, API_HASH)
 
 
+@dataclass
+class User:
+    id: int
+    username: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    phone: str | int | None = None
+
+    @staticmethod
+    def user_fields() -> list:
+        return [field.name for field in fields(User)]
+
+
 async def main():
     chats = []
 
@@ -29,8 +44,31 @@ async def main():
     )
 
     chats.extend(result.chats)
-    list_title = [chat.title for chat in chats]
-    print(list_title)
+
+    for i, chat in enumerate(chats):
+        print(f"Index group: {i}; Name group: {chat.title}")
+    print("*" * 100 + "\nChoose a group")
+
+    group_numb = input("Enter index group: ")
+    all_participants = await client.get_participants(
+        chats[int(group_numb)], aggressive=True
+    )
+
+    print("*" * 100 + "\nSTART")
+
+    users_list = []
+    for user in all_participants:
+        users_list.append(
+            User(
+                id=user.id,
+                username=user.username if user.username else None,
+                first_name=user.first_name if user.first_name else None,
+                last_name=user.last_name if user.last_name else None,
+                phone=user.phone if user.phone else None,
+            )
+        )
+
+    pprint(users_list)
 
 
 with client:
