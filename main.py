7b6e06284e8e6e3e16ed67f0a959dd6df2e0a298
Maksym Bezhevets <begik4@gmail.com>
2024-02-22 13:48:46 +0200

    Writing results to csv

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import csv
 import os
-from dataclasses import dataclass, fields
-from pprint import pprint
+from dataclasses import dataclass, fields, asdict
 from typing import List
 
+from slugify import slugify
 from telethon import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
@@ -25,10 +26,22 @@ class User:
     first_name: str | None = None
     last_name: str | None = None
     phone: str | int | None = None
+    was_online: str | None = None
 
     @staticmethod
     def user_fields() -> list:
         return [field.name for field in fields(User)]
+
+
+async def write_to_csv(file_name: str, users_list: List[User]) -> None:
+    with open(
+        f"{slugify(file_name)}-members.csv", "w", encoding="utf-8", newline=""
+    ) as file:
+        writer = csv.DictWriter(file, fieldnames=User.user_fields())
+
+        writer.writeheader()
+        for user in users_list:
+            writer.writerow(asdict(user))
 
 
 async def get_all_chat() -> list:
@@ -54,17 +67,21 @@ async def choice_of_chat(chats: list):
     print("*" * 100 + "\nChoose a group")
 
     group_numb = input("Enter index group: ")
+    print("*" * 100 + "\nSTART")
     all_participants = await client.get_participants(
         chats[int(group_numb)], aggressive=True
     )
-    return all_participants
+    return all_participants, chats[int(group_numb)].title
 
 
 async def get_all_members_group(all_participants) -> List[User]:
-    print("*" * 100 + "\nSTART")
 
     users_list = []
     for user in all_participants:
+        try:
+            was_online = user.status.was_online.strftime("%d-%m-%Y")
+        except AttributeError:
+            was_online = None
         users_list.append(
             User(
                 id=user.id,
@@ -72,6 +89,7 @@ async def get_all_members_group(all_participants) -> List[User]:
                 first_name=user.first_name if user.first_name else None,
                 last_name=user.last_name if user.last_name else None,
                 phone=user.phone if user.phone else None,
+                was_online=was_online,
             )
         )
     return users_list
@@ -79,11 +97,11 @@ async def get_all_members_group(all_participants) -> List[User]:
 
 async def main() -> None:
     chats = await get_all_chat()
-
-    all_participants = await choice_of_chat(chats)
-
+    all_participants, chat_name = await choice_of_chat(chats)
     list_users = await get_all_members_group(all_participants)
-    pprint(list_users)
+
+    await write_to_csv(file_name=chat_name, users_list=list_users)
+    print("All user scraped\n" + "*" * 100)
 
 
 with client:
